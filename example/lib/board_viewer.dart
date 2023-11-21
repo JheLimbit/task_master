5aef70af4a031c004195c51fe5038cdafc72744b
Jerry He <118411012+jerhe3@users.noreply.github.com>
2023-11-21 12:53:08 -0500

    Trying to integrate chart into board manager

--- example/lib/board_viewer.dart
+++ example/lib/board_viewer.dart
@@ -832,6 +832,9 @@ class _BoardViewerState extends State<BoardViewer> {
                     cards: currentCardData,
                     users: createDropDown(),
                     usersProfiles: widget.users,
+                    completedTasks: completedTasks != null
+                        ? completedTasks[currentEpic]
+                        : null,
                     currentUser:
                         UserData.fromJSON(widget.users[widget.currentUID])),
                 selectedProject == ''
